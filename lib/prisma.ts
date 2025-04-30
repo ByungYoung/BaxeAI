d0916c01b5dcd3e076d2e242804084df2c25f673
Beongyoung Park <sergiopark10373@likelion.org>
2025-05-01 00:24:40 +0900

    refactor: Prisma 클라이언트 타입 정의를 InstanceType에서 사용자 정의 타입으로 변경 및 인스턴스 생성 방식 수정

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,17 @@
 // prisma.ts - Prisma 클라이언트 싱글톤 패턴 적용
 import * as prisma from "@prisma/client";
 
-// 클라이언트 생성자 함수
-const PrismaClient = prisma.PrismaClient;
+// 클라이언트 타입 및 인스턴스 정의
+type PrismaClientType = any;
+const prismaClient = prisma as unknown as { default: PrismaClientType };
 
 // 전역 객체 타입 확장
 declare global {
-  var prisma: InstanceType<typeof PrismaClient> | undefined;
+  var prisma: PrismaClientType | undefined;
 }
 
 // 글로벌 싱글톤 prisma 인스턴스 생성
-const client = global.prisma || new PrismaClient();
+const client = global.prisma || new prismaClient.default();
 
 // 개발 환경에서만 전역 변수에 할당 (핫 리로딩 방지)
 if (process.env.NODE_ENV !== "production") {
